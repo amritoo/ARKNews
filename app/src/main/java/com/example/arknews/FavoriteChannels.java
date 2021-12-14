425b9b2475b3d0bdf5696cb415fd92f547391cc2
karmajimba98 <karmadurant35@gmail.com>
2021-12-14 21:53:55 +0600

    updated favorite channels and categories

---
+++ app/src/main/java/com/example/arknews/FavoriteChannels.java
@@ -0,0 +1,16 @@
+package com.example.arknews;
+
+import static com.example.arknews.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoriteChannels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_favorite_channels);
+    }
+}
---
+++ app/src/main/java/com/example/arknews/categories/CategorySelectionActivity.java
@@ -0,0 +1,21 @@
+package com.example.arknews.categories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.arknews.R;
+
+public class CategorySelectionActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_selection);
+
+
+    }
+}
