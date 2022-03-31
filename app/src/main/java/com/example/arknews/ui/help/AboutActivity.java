e52ddc391b1b04c821c606437af2df8d83e36436
karmajimba23 <karmadurant35@gmail.com>
2022-03-31 11:03:20 +0600

    about updated

--- app/src/main/java/com/example/arknews/ui/help/AboutActivity.java
+++ app/src/main/java/com/example/arknews/ui/help/AboutActivity.java
@@ -3,14 +3,26 @@ package com.example.arknews.ui.help;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 import com.example.arknews.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AboutActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        toolbar = findViewById(R.id.about_toolbar);
+        scrollView = findViewById(R.id.menu_about);
+
+        toolbar.setNavigationOnClickListener(v -> finish());
+
     }
 }
